a12273ec6ff3c26d6e2f84adb656359ccd671e12
Andy <anhnguyen.professional@gmail.com>
2026-01-21 13:48:29 -0600

    chore: dont render element while unAuth

--- src/components/RequireAuth.tsx
+++ src/components/RequireAuth.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { useIsAuthenticated, useMsal } from "@azure/msal-react";
+import { Box, CircularProgress } from "@mui/material";
 
 type Prop = {
   children: React.ReactElement;
@@ -9,13 +10,22 @@ export default function RequireAuth({ children }: Prop) {
   const isAuthenticated = useIsAuthenticated();
   const { instance, inProgress } = useMsal();
 
+  const msalIsBusy = inProgress !== "none";
+
   React.useEffect(() => {
-    if (!isAuthenticated && inProgress === "none") {
+    if (!isAuthenticated && !msalIsBusy) {
       instance.loginRedirect({
         scopes: ["openid", "profile", "email"],
       });
     }
-  }, [isAuthenticated, inProgress, instance]);
+  }, [isAuthenticated, msalIsBusy, instance]);
 
+  if (!isAuthenticated) {
+    return (
+      <Box sx={{ display: "grid", placeItems: "center", minHeight: "40vh" }}>
+        <CircularProgress />
+      </Box>
+    );
+  }
   return children;
 }
